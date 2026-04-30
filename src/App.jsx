8d5924fe4d958e07cedf84ca0aaee5c0bbf7728c
JOÃO PEDRO LOPES <joao_pedro-lopes@estudante.sesisenai.org.br>
2026-04-30 11:51:29 -0300

    m

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './paginas/Login'
+import Cadastro from './paginas/Cadastro'
+import Usuario from './paginas/Usuario'
+
 
 function App() {
+const [tela, setTela] = useState('login')
+
  const renderizarTela = () => {
-  //navegação
+  if(tela === 'login') {
+    return <Login/>
+  } else if (tela === 'cadastro') {
+    return <Cadastro/>
+  } else if (tela === 'usuario'){
+   return <Usuario/>
+  }
  }
+
+const trocarTela = (pagina) => {
+  setTela(pagina)
+}
+
   return (
     <>
-      <section id="center">
-        
-      </section>
+      <h1>pikachu</h1>
+      <button onClick={ () => trocarTela('login')}>login</button>
+      <button onClick={ () => trocarTela('cadastro')}>cadastro</button>
+      <button onClick={ () => trocarTela('usuario')}>usuario</button>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      {renderizarTela()}
     </>
   )
 }
